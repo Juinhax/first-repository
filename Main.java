aa193413a066f9c4a9ebb3c835e8fa9f7efa88b6
Juinhax <106928755+Juinhax@users.noreply.github.com>
2023-01-30 17:41:28 -0300

    idade-do-mo

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
-package pratica_1;
+package idade_do_mo;
 
+import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		int y = 32;
-		System.out.println(y);
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("digite o ano do mo: ");
+		int ano = sc.nextInt();
+		System.out.print("digite o ano atual: ");
+		int ano2 = sc.nextInt();
+		int subtracao = (ano2 - ano);
+		// = : se lê recebe
+		System.out.println(subtracao);
+		
+		sc.close();
+		
 	}
 }
